e655726d048972a2321f8872647602551a31e9f9
Pablo <polecito.em@gmail.com>
2015-10-13 12:17:43 +0000

    Working proxy

--- proxy.js
+++ proxy.js
@@ -1,11 +1,27 @@
-var http = require('http')
-, httpProxy = require('http-proxy');
+var http = require('http'),
+    httpProxy = require('http-proxy'),
+    proxy = httpProxy.createProxyServer({}),
+    url = require('url');
 
-httpProxy.createServer({
-  hostnameOnly: true,
-  router: {
-    'www.hanja.me': '127.0.0.1:8080',
-    'hanja.me': '127.0.0.1:8080',
-    'chess.iampablo.me' : '127.0.0.1:8081'
-  }
-}).listen(80);
+http.createServer(function(req, res) {
+    var hostname = req.headers.host.split(":")[0];
+    var pathname = url.parse(req.url).pathname;
+
+    console.log(pathname);
+    switch(hostname)
+    {
+        case 'www.hanja.me':
+            proxy.web(req, res, {target: 'http://0.0.0.0:8081'});
+            break;
+        case 'hanja.me':
+            proxy.web(req, res, {target: 'http://0.0.0.0:8081'});
+            break;
+        case 'chess.iampablo.me':
+            proxy.web(req,res, {target: 'http://0.0.0.0:8080'});
+        default:
+            proxy.web(req, res, {target: 'http://0.0.0.0:8081'});
+    }
+}).listen(80, function() {
+    console.log('proxy listening on port 80');
+});
+
